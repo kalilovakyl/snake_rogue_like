af4febe4a5677f5f66aae884df4123e3be5da0f8
Akyl <kalilovakylbek56@gmail.com>
2026-06-29 21:45:42 +0500

    added border between game and items

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "raylib.h"
 #include <stdio.h>
 
+const int st_pt_x = 0;
+const int st_pt_y = 200;
+
 typedef struct {
     int w;
     int h;
@@ -29,8 +32,8 @@ int main(void) {
     ToggleFullscreen();
 
     Player p1;
-    p1.x = 0;
-    p1.y = 0;
+    p1.x = st_pt_x;
+    p1.y = st_pt_y;
     p1.width = 40;
     p1.speed = 800.0f;
     p1.color = WHITE;
@@ -43,6 +46,9 @@ int main(void) {
 
         BeginDrawing();
         ClearBackground(BLACK);
+        
+        int thickness = 20;
+        DrawLineEx((Vector2){ st_pt_x - thickness, st_pt_y - thickness }, (Vector2){ srn.w, st_pt_y -thickness }, thickness, BLUE);
 
 	    DrawRectangle(p1.x, p1.y, p1.width, p1.width, p1.color);
 
@@ -57,15 +63,15 @@ void input_handling(Player *p1, float dt, Screen srn) {
   float fatness_w = srn.w - p1->width;
   float fatness_y = srn.h - p1->width;
 
-  if (p1->x >= 0 && p1->x <= fatness_w && p1->y >= 0 && p1->y <= fatness_y) {
+  if (p1->x >= st_pt_x && p1->x <= fatness_w && p1->y >= st_pt_y && p1->y <= fatness_y) {
       if (IsKeyDown(KEY_D)) p1->x += p1->speed * dt;
       if (IsKeyDown(KEY_A)) p1->x -= p1->speed * dt;
       if (IsKeyDown(KEY_W)) p1->y -= p1->speed * dt;
       if (IsKeyDown(KEY_S)) p1->y += p1->speed * dt;
   } else {
-	if (p1->x < 0) p1->x = 0;
+	if (p1->x < st_pt_x) p1->x = st_pt_x;
 	if (p1->x > fatness_w) p1->x = fatness_w;
-	if (p1->y < 0) p1->y = 0;
+	if (p1->y < st_pt_y) p1->y = st_pt_y;
 	if (p1->y > fatness_y) p1->y = fatness_y;
   }
 }
